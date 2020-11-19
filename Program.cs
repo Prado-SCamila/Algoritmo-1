22a82c374cfd8cb8e3a0050ff2766478660b6793
Prado-SCamila <prado.camila.910423@gmail.com>
2020-11-19 12:48:17 -0300

    Fim

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Algoritmo_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Digite um número inteiro de 0 a 10.");
+            int num = int.Parse(Console.ReadLine());
+
+            while(num<0 || num>10){
+            Console.WriteLine("Número inválido");
+            Console.WriteLine("Digite um número entre 0 e 10.");
+            num = int.Parse(Console.ReadLine());
+            
+            }
+
+        }
+    }
+
+}
